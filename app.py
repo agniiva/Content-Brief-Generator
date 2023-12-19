64a9f8e9165e404154c21bac6e4211a0ac121cfa
Agniva Mahata <73607864+agniiva@users.noreply.github.com>
2023-12-20 04:43:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ async def generate_content_brief(data: KeywordData):
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=10000)
